9927efcef347ea352cb3ef0a8fb2dd92e4745a25
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2021-09-22 20:59:29 -0300

    update

--- challenge-01/challenge/cpf_after.js
+++ challenge-01/challenge/cpf_after.js
@@ -1,64 +1,53 @@
-const regex = /[.-]/gi
+function haveMoreElevenNumber(number) {
+  const test = number.length >= 11 || number.length <= 14 ? 'Num tem' : 'Tem'
+  return console.log(test.split("").every(c => c === test[0]))
+} 
 
 function validate(str) {
-  if (str !== null) {
-    if (str !== undefined) {
-      if (str.length >= 11 || str.length <= 14) {
-        
-        console.log('primeiro', str)
-        str = str.replace(regex, '')
+  const regex = /[.-]/gi
 
-        if (!str.split("").every(c => c === str[0])) {
-          console.log('Segundo', str)
+  if (str.length >= 11 || str.length <= 14) {
+    str = str.replace(regex, '')
 
-          try {
-            let d1, d2;
-            let dg1, dg2, rest;
-            let digito;
-            let nDigResult;
-            d1 = d2 = 0;
-            dg1 = dg2 = rest = 0;
+    if (!str.split("").every(c => c === str[0])) {
 
-            for (let nCount = 1; nCount < str.length - 1; nCount++) {
-              // if (isNaN(parseInt(str.substring(nCount -1, nCount)))) {
-              // 	return false;
-              // } else {
+      try {
+        let d1, d2;
+        let dg1, dg2, rest;
+        let digito;
+        let nDigResult;
+        d1 = d2 = 0;
+        dg1 = dg2 = rest = 0;
 
-              digito = parseInt(str.substring(nCount - 1, nCount));
-              d1 = d1 + (11 - nCount) * digito;
+        for (let nCount = 1; nCount < str.length - 1; nCount++) {
+          digito = parseInt(str.substring(nCount - 1, nCount));
+          d1 = d1 + (11 - nCount) * digito;
 
-              d2 = d2 + (12 - nCount) * digito;
-              // }
-            };
+          d2 = d2 + (12 - nCount) * digito;
+        };
 
-            rest = (d1 % 11);
+        rest = (d1 % 11);
 
-            dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;
-            d2 += 2 * dg1;
-            rest = (d2 % 11);
-            if (rest < 2)
-              dg2 = 0;
-            else
-              dg2 = 11 - rest;
+        dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;
+        d2 += 2 * dg1;
+        rest = (d2 % 11);
+        if (rest < 2)
+          dg2 = 0;
+        else
+          dg2 = 11 - rest;
 
-            let nDigVerific = str.substring(str.length - 2, str.length);
-            nDigResult = "" + dg1 + "" + dg2;
-            return nDigVerific == nDigResult;
-          } catch (e) {
-            console.error("Erro !" + e);
+        let nDigVerific = str.substring(str.length - 2, str.length);
+        nDigResult = "" + dg1 + "" + dg2;
+        return nDigVerific == nDigResult;
+      } catch (e) {
+        console.error("Erro !" + e);
 
-            return false;
-          }
-        } else return false
-
-      } else return false;
-    }
-
-
-  } else return false;
-
+        return false;
+      }
+    } else return false
+  }
 }
-
+console.log(haveMoreElevenNumber("111.111.111-111111"));
 console.log(validate("111.111.111-11"));
 console.log(validate("123.456.789-99"));
 console.log(validate("935.411.347-80"));
---
+++ challenge-01/challenge/cpf_after.test.js
@@ -0,0 +1,3 @@
+test("Deve funcionar", () => {
+  expect(1).toBe(1)
+});
