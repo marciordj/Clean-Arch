d23677568cf18581c1b1b24f8af00b9b1ceebc35
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2021-09-24 21:41:40 -0300

    new configs

--- challenge-01/jest.config.js
+++ challenge-01/jest.config.js
@@ -1,6 +1,10 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  rootDir: 'src',
-  preset: 'ts-jest',
-  testEnvironment: 'node'
-};
+  roots: ['<rootDir>/src'],
+  testEnvironment: 'node',
+  transform: {
+      '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+      '@/(.*)': '<rootDir>/src/$1'
+  }
+}
--- challenge-01/src/cpf_after.ts
+++ challenge-01/src/cpf_after.ts
@@ -1,50 +1,57 @@
-function haveMoreElevenNumber(number: string) {
-  const test = number.length >= 11 || number.length <= 14 ? 'Num tem' : 'Tem';
-  return console.log(test.split('').every(c => c === test[0]));
+const regex = /[.-]/gi;
+const CPF_LENGTH = 11;
+
+export function validCpfNumber(number: string) {
+  const numberReplaced = number.replace(regex, '');
+
+  return numberReplaced.length === CPF_LENGTH ?? false;
 }
 
-function validate(str) {
-  const regex = /[.-]/gi;
+console.log('1: ', validCpfNumber('450.238.008-30'));
+console.log('2: ', validCpfNumber('450.2382.008-30'));
 
-  if (str.length >= 11 || str.length <= 14) {
-    str = str.replace(regex, '');
+// function validate(str) {
+//   const regex = /[.-]/gi;
 
-    if (!str.split('').every(c => c === str[0])) {
-      try {
-        let d1, d2;
-        let dg1, dg2, rest;
-        let digito;
-        let nDigResult;
-        d1 = d2 = 0;
-        dg1 = dg2 = rest = 0;
+//   if (str.length >= 11 || str.length <= 14) {
+//     str = str.replace(regex, '');
 
-        for (let nCount = 1; nCount < str.length - 1; nCount++) {
-          digito = parseInt(str.substring(nCount - 1, nCount));
-          d1 = d1 + (11 - nCount) * digito;
+//     if (!str.split('').every(c => c === str[0])) {
+//       try {
+//         let d1, d2;
+//         let dg1, dg2, rest;
+//         let digito;
+//         let nDigResult;
+//         d1 = d2 = 0;
+//         dg1 = dg2 = rest = 0;
 
-          d2 = d2 + (12 - nCount) * digito;
-        }
+//         for (let nCount = 1; nCount < str.length - 1; nCount++) {
+//           digito = parseInt(str.substring(nCount - 1, nCount));
+//           d1 = d1 + (11 - nCount) * digito;
 
-        rest = d1 % 11;
+//           d2 = d2 + (12 - nCount) * digito;
+//         }
 
-        dg1 = rest < 2 ? (dg1 = 0) : 11 - rest;
-        d2 += 2 * dg1;
-        rest = d2 % 11;
-        if (rest < 2) dg2 = 0;
-        else dg2 = 11 - rest;
+//         rest = d1 % 11;
 
-        const nDigVerific = str.substring(str.length - 2, str.length);
-        nDigResult = '' + dg1 + '' + dg2;
-        return nDigVerific == nDigResult;
-      } catch (e) {
-        console.error('Erro !' + e);
+//         dg1 = rest < 2 ? (dg1 = 0) : 11 - rest;
+//         d2 += 2 * dg1;
+//         rest = d2 % 11;
+//         if (rest < 2) dg2 = 0;
+//         else dg2 = 11 - rest;
 
-        return false;
-      }
-    } else return false;
-  }
-}
-console.log(haveMoreElevenNumber('111.111.111-111111'));
-console.log(validate('111.111.111-11'));
-console.log(validate('123.456.789-99'));
-console.log(validate('935.411.347-80'));
+//         const nDigVerific = str.substring(str.length - 2, str.length);
+//         nDigResult = '' + dg1 + '' + dg2;
+//         return nDigVerific == nDigResult;
+//       } catch (e) {
+//         console.error('Erro !' + e);
+
+//         return false;
+//       }
+//     } else return false;
+//   }
+// }
+// console.log(haveMoreElevenNumber('111.111.111-111111'));
+// console.log(validate('111.111.111-11'));
+// console.log(validate('123.456.789-99'));
+// console.log(validate('935.411.347-80'));
