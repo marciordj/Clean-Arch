fe4ad3a47dc83cded11e3fee5d8cb29b3bcb8223
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2021-10-05 21:27:47 -0300

    update

--- project/src/Order.ts
+++ project/src/Order.ts
@@ -7,37 +7,39 @@ export default class Order {
     if (!this.validate(cpf)) throw new Error('Invalid cpf')
   }
 
-  cleanCpf(cpf: string) {
-    return cpf.replace(/\D/g, '');
-  }
 
-  areAllDigitsEqual(cpf: string) {
+ cleanCpf (cpf: string) {
+    return cpf.replace(/\D/g, "");
+}
+
+ areAllDigitsEqual (cpf: string) {
     const [firstDigit] = cpf;
     return [...cpf].every(c => c === firstDigit);
-  }
+}
 
-  calculateDigit(cpf: string, factor: number) {
+ calculateDigit (cpf: string, factor: number) {
     let total = 0;
     for (const digit of cpf) {
-      if (factor > 1) total += parseInt(digit) * factor--
+        if (factor > 1) total += parseInt(digit) * factor--;
     }
-  }
+    const rest = total%11;
+    return (rest < 2) ? 0 : (11 - rest);
+}
 
-  extractVerifierDigit(cpf: string) {
-    return cpf.slice(9)
-  }
+ extractVerifierDigit (cpf: string) {
+    return cpf.slice(9);
+}
 
-  validate(rawCpf: any) {
-    if (!rawCpf) return false;
+ validate (rawCpf: any) {
+	if (!rawCpf) return false;
     const cpf = this.cleanCpf(rawCpf);
     if (cpf.length !== this.CPF_VALID_LENGTH) return false;
     if (this.areAllDigitsEqual(cpf)) return false;
-    const firstVerifiedDigit = this.calculateDigit(cpf, this.FACTOR_FIRST_VERIFIER_DIGIT);
-    const secondVerifiedDigit = this.calculateDigit(cpf, this.FACTOR_SECOND_VERIFIER_DIGIT);
-    const verifiedDigit = this.extractVerifierDigit(cpf);
-    const calculatedVerifiedDigit = `${firstVerifiedDigit}${secondVerifiedDigit}`;
-
-    return verifiedDigit === calculatedVerifiedDigit;
-  }
+    const firstVerifierDigit = this.calculateDigit(cpf, this.FACTOR_FIRST_VERIFIER_DIGIT);
+    const secondVerifierDigit = this.calculateDigit(cpf, this.FACTOR_SECOND_VERIFIER_DIGIT);
+    const verifierDigit = this.extractVerifierDigit(cpf);  
+    const calculatedVerifiedDigit = `${firstVerifierDigit}${secondVerifierDigit}`;  
+    return verifierDigit === calculatedVerifiedDigit;
+}
 
 }
--- project/test/Order.test.ts
+++ project/test/Order.test.ts
@@ -3,3 +3,8 @@ import Order from "../src/Order";
 test("Should be create an order with invalid cpf", () => {
   expect(() => new Order("111.111.111-11")).toThrow(new Error("Invalid cpf"));
 })
+
+test("Should be create an order", () => {
+  const order = new Order("450-238-008-30");
+  expect(order).toBeDefined();
+})
