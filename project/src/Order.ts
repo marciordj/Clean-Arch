372af22d81d6838a75ebae4e5000f51237120ced
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2021-10-05 21:47:54 -0300

    create addItem and geTotal method

--- project/src/Order.ts
+++ project/src/Order.ts
@@ -1,45 +1,25 @@
+import Cpf from "./Cpf";
+
 export default class Order {
-  CPF_VALID_LENGTH = 11;
-  FACTOR_FIRST_VERIFIER_DIGIT = 10;
-  FACTOR_SECOND_VERIFIER_DIGIT = 11;
+  cpf: Cpf;
+  orderItems: OrderItem[];
 
   constructor (cpf: string) {
-    if (!this.validate(cpf)) throw new Error('Invalid cpf')
+    this.cpf = new Cpf(cpf);
+    this.orderItems = []
   }
 
+  addItem(item: Item, quantity: number) {
+    this.orderItems.push(new OrderItem(item.idItem, item.price, quantity))
+  }
 
- cleanCpf (cpf: string) {
-    return cpf.replace(/\D/g, "");
-}
-
- areAllDigitsEqual (cpf: string) {
-    const [firstDigit] = cpf;
-    return [...cpf].every(c => c === firstDigit);
-}
-
- calculateDigit (cpf: string, factor: number) {
+  getTotal() {
     let total = 0;
-    for (const digit of cpf) {
-        if (factor > 1) total += parseInt(digit) * factor--;
+    for (const orderItem of this.orderItems) {
+      total += orderItem.price * orderItem.quantity
     }
-    const rest = total%11;
-    return (rest < 2) ? 0 : (11 - rest);
-}
 
- extractVerifierDigit (cpf: string) {
-    return cpf.slice(9);
-}
-
- validate (rawCpf: any) {
-	if (!rawCpf) return false;
-    const cpf = this.cleanCpf(rawCpf);
-    if (cpf.length !== this.CPF_VALID_LENGTH) return false;
-    if (this.areAllDigitsEqual(cpf)) return false;
-    const firstVerifierDigit = this.calculateDigit(cpf, this.FACTOR_FIRST_VERIFIER_DIGIT);
-    const secondVerifierDigit = this.calculateDigit(cpf, this.FACTOR_SECOND_VERIFIER_DIGIT);
-    const verifierDigit = this.extractVerifierDigit(cpf);  
-    const calculatedVerifiedDigit = `${firstVerifierDigit}${secondVerifierDigit}`;  
-    return verifierDigit === calculatedVerifiedDigit;
-}
+    return total
+  }
 
 }
--- project/test/Order.test.ts
+++ project/test/Order.test.ts
@@ -2,9 +2,18 @@ import Order from "../src/Order";
 
 test("Should be create an order with invalid cpf", () => {
   expect(() => new Order("111.111.111-11")).toThrow(new Error("Invalid cpf"));
-})
+});
 
 test("Should be create an order", () => {
   const order = new Order("450-238-008-30");
   expect(order).toBeDefined();
-})
+});
+
+test("Should be create an order with 3 itens", () => {
+  const order = new Order("450-238-008-30")
+  order.addItem(new Item(1, "Colecionaveis", "Manopla do infinito", 2000), 1);
+  order.addItem(new Item(2, "Colecionaveis", "Joia da alma", 2200), 1);
+  order.addItem(new Item(3, "Colecionaveis", "Joia do poder", 5000), 1);
+  const total = order.getTotal();
+  expect(total).toBe(9200)
+});
